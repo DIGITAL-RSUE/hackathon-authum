b9781297cb413aa27d126f82caf533485951de3a
hackkikchak <german.serchaevk2.k2@yandex.ru>
2022-10-23 12:13:49 +0300

    include urls nested category

--- backend/apps/api/base/v1/routers.py
+++ backend/apps/api/base/v1/routers.py
@@ -10,6 +10,7 @@ router = DefaultRouter()
 
 # EXHIBITOR URLS REGISTER
 router.register(r'exhibitor/category', views.ExhibitorCategoryViewSet)
+router.register(r'exhibitor/category-nested', views.ExhibitorCategoryNestedViewSet)
 router.register(r'exhibitor/case', views.ExhibitorCaseViewSet)
 router.register(r'exhibitor/comment', views.ExhibitorCommentViewSet)
 router.register(r'exhibitor/location', views.ExhibitorLocationViewSet)
@@ -18,6 +19,7 @@ router.register(r'exhibitor/product', views.ExhibitorProductViewSet)
 
 # MODETATOR URLS REGISTER
 router.register(r'moderator/category/(?P<exhibitor_id>\d+)', views.ModeratorCategoryViewSet)
+router.register(r'moderator/category-nested/(?P<exhibitor_id>\d+)', views.ModeratorCategoryNestedViewSet)
 router.register(r'moderator/case/(?P<exhibitor_id>\d+)', views.ModeratorCaseViewSet)
 router.register(r'moderator/comment/(?P<exhibitor_id>\d+)', views.ModeratorCommentViewSet)
 router.register(r'moderator/location/(?P<exhibitor_id>\d+)', views.ModeratorLocationViewSet)
