e7ffa516f01bbf01fbf8481231dda5e6a234923f
penkhasoveg <pen.egor2002@gmail.com>
2022-10-23 13:51:04 +0300

    feat(api): auth api

--- backend/apps/core/api/v1/urls.py
+++ backend/apps/core/api/v1/urls.py
@@ -4,4 +4,6 @@ app_name = "v1"
 
 urlpatterns = [
     path("base/", include("apps.api.base.v1")),
+    path("auth/", include("dj_rest_auth.urls")),
+    path("auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
