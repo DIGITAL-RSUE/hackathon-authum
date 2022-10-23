b67ce6988bad13172e8b78a92e630bd054c2f217
hackkikchak <german.serchaevk2.k2@yandex.ru>
2022-10-23 12:31:37 +0300

    add product pagination, add product filters

--- backend/apps/api/base/v1/views/product_view_set.py
+++ backend/apps/api/base/v1/views/product_view_set.py
@@ -1,15 +1,39 @@
+from django_filters import rest_framework as filters
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 
 from ...models import Product
 from ..serializers import ProductSerializer
 
 
+class ProductFilter(filters.FilterSet):
+
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    class Meta:
+        model = Product
+        exclude = ["video",]
+
+class ProductPagination(PageNumberPagination):
+
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+
 class ExhibitorProductViewSet(viewsets.ModelViewSet):
+
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    pagination_class = ProductPagination
+    filterset_class = ProductFilter
 
 
 class ModeratorProductViewSet(viewsets.ModelViewSet):
+
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    pagination_class = ProductPagination
+    filterset_class = ProductFilter
